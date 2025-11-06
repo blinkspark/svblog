4d422e927647620e9444c61d292519447e001a76
Blinkspark <wn@nw.com>
2025-11-06 10:37:47 +0800

    Replace login logic with posts listing and pagination

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,48 @@
 <script lang="ts">
-  import { pb } from '$lib'
+  import { pb, POSTS_PER_PAGE } from '$lib'
   import { onMount } from 'svelte'
-  let isLoggedIn = $state(false)
-  $effect(() => {
-    const unsub = pb.authStore.onChange((auth) => {
-      isLoggedIn = pb.authStore.isValid
-    })
-    isLoggedIn = pb.authStore.isValid
-    return unsub
-  })
-  onMount(() => {
-    let theme = localStorage.getItem('theme')
-    if (!theme) {
-      localStorage.setItem('theme', 'dark')
-      theme = 'dark'
+  import { Markdown } from 'svelte-exmarkdown'
+
+  let currentPage = $state(1)
+  let totalPages = $state(1)
+  let posts = $state([]) as Array<any>
+
+  let isFetchingPage = $state(false)
+
+  async function fetchPage() {
+    if (isFetchingPage) return
+    isFetchingPage = true
+
+    try {
+      let res = await pb.collection('posts').getList(currentPage, POSTS_PER_PAGE)
+      totalPages = res.totalPages
+      posts = res.items
+    } catch (error) {
+    } finally {
+      isFetchingPage = false
     }
-    document.documentElement.setAttribute('data-theme', theme)
+  }
+
+  onMount(() => {
+    fetchPage()
   })
 </script>
 
+<div class="container">
+  {#if isFetchingPage}
+    <h1 class="title mt-3">Loading...</h1>
+  {/if}
+  {#each posts as post}
+    <div class="card mt-3">
+      <header class="card-header has-background-primary">
+        <p class="card-header-title is-size-3">{post.title}</p>
+      </header>
+      <div class="content p-5">
+        <Markdown md={post.content} />
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+</style>
